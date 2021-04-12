8df8dc7de4fa6c42c746178755b8518e58c7e4ad
xeusteerapat <xeus085@gmail.com>
2021-04-12 21:19:54 +0700

    [EDIT] update sequelize config

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,27 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+// import { Sequelize } from 'sequelize-typescript';
+// import { config } from './config/config';
 
+// export const sequelize = new Sequelize({
+//   host: config.host,
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   port: 5432,
+//   dialect: config.dialect,
+//   storage: ':memory:',
+// });
+
+import { Sequelize } from 'sequelize-typescript';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
-
-  'dialect': config.dialect,
-  'storage': ':memory:',
+  host: process.env.POSTGRES_HOST,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: Number(process.env.POSTGRES_PORT),
+  dialect: 'postgres',
+  storage: ':memory:',
 });
